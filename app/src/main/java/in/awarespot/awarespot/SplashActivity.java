62ef53534161a5d47ce11d655093c37e14a5473c
sai <saisondarkar18@gmail.com>
2017-06-24 11:21:22 +0530

    Splash Acitvity Updated

--- app/src/main/java/in/awarespot/awarespot/SplashActivity.java
+++ app/src/main/java/in/awarespot/awarespot/SplashActivity.java
@@ -1,14 +1,76 @@
 
 package in.awarespot.awarespot;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
+
+    protected boolean _active = true;
+    protected int _splashTime = 1000; // time to display the splash screen in ms
+    public boolean is_first = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        screendesign();
+
+
     }
+
+    public void screendesign(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = getWindow().getDecorView();
+
+            boolean shouldChangeStatusBarTintToDark = false;
+
+            if (shouldChangeStatusBarTintToDark) {
+                decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            } else {
+                // We want to change tint color to white again.
+                // You can also record the flags in advance so that you can turn UI back completely if
+                // you have set other flags before, such as translucent or full screen.
+                decor.setSystemUiVisibility(0);
+            }
+        }
+    }
+
+    public void splashThread(){
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if (_active) {
+                            waited += 100;
+                            if(waited>_splashTime-200)
+                            {
+
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+
+                } finally {                    {
+
+                    startActivity(new Intent(SplashActivity.this,
+                            LoginActivity.class));}
+
+                    finish();
+                }
+            };
+        };
+
+        splashTread.start();
+
+    }
+
 }
