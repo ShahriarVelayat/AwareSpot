04b85c8d76262031264f50a7fed69faf9dde055e
sai <saisondarkar18@gmail.com>
2017-06-24 12:06:17 +0530

    User Profile activty added and Maps Activity added

--- app/src/main/java/in/awarespot/awarespot/activities/UserProfileActivity.java
+++ app/src/main/java/in/awarespot/awarespot/activities/UserProfileActivity.java
@@ -1,15 +1,189 @@
 package in.awarespot.awarespot.activities;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import in.awarespot.awarespot.R;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+
+    private final static String TAG = "UserProfileActivity";
+    private EditText nameEditText;
+    private Button doneButton, addNewButton;
+    private ListView cityListVIew;
+    private String uid;
+    private TextView attributeTextView;
+
+    private Geocoder mGeocoder;
+
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        initUiElements();
+        initUiListners();
+        getuser();
+
     }
+
+    public void initUiElements() {
+        nameEditText = (EditText) findViewById(R.id.input_name);
+        attributeTextView = (TextView) findViewById(R.id.attribute);
+        doneButton  = (Button) findViewById(R.id.btn_done);
+        addNewButton = (Button) findViewById(R.id.btn_addnew);
+        cityListVIew = (ListView) findViewById(R.id.listViewAndroid);
+
+        mGeocoder = new Geocoder(UserProfileActivity.this, Locale.getDefault());
+    }
+
+    public void initUiListners(){
+
+        addNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAutocompleteActivity();
+            }
+        });
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void getuser(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            uid = user.getUid();
+            Log.d(TAG,uid);
+        } else {
+            // No user is signed in
+        }
+    }
+
+    private void openAutocompleteActivity() {
+        try {
+            // The autocomplete activity requires Google Play Services to be available. The intent
+            // builder checks this and throws an exception if it is not the case.
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(UserProfileActivity.this);
+            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // Indicates that Google Play Services is either not installed or not up to date. Prompt
+            // the user to correct the issue.
+            GoogleApiAvailability.getInstance().getErrorDialog(UserProfileActivity.this, e.getConnectionStatusCode(),
+                    0 /* requestCode */).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // Indicates that Google Play Services is not available and the problem is not easily
+            // resolvable.
+            String message = "Google Play Services is not available: " +
+                    GoogleApiAvailability.getInstance().getErrorString(e.errorCode);
+
+            Log.e(TAG, message);
+            Toast.makeText(UserProfileActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check that the result was from the autocomplete widget.
+        if (requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            if (resultCode == RESULT_OK) {
+                // Get the user's selected place from the Intent.
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i(TAG, "Place Selected: " + place.getLatLng());
+                Double lat = place.getLatLng().latitude;
+                Double lng = place.getLatLng().longitude;
+
+                try{
+
+                    Log.i(TAG, "City Selected: " + getCityNameByCoordinates(lat,lng));
+                    Log.i(TAG, "City Selected: " + getCityNameByCoordinates(lat,lng));
+                }catch (Exception e){
+
+                }
+
+                // Format the place's details and display them in the TextView.
+                nameEditText.setText(formatPlaceDetails(getResources(), place.getName(),
+                        place.getId(), place.getAddress(), place.getPhoneNumber(),
+                        place.getWebsiteUri()));
+                attributeTextView.setText(""+place.getLocale());
+
+                // Display attributions if required.
+                CharSequence attributions = place.getAttributions();
+                if (!TextUtils.isEmpty(attributions)) {
+                    attributeTextView.setText(Html.fromHtml(attributions.toString()));
+                } else {
+                    attributeTextView.setText("");
+                }
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.e(TAG, "Error: Status = " + status.toString());
+            } else if (resultCode == RESULT_CANCELED) {
+                // Indicates that the activity closed before a selection was made. For example if
+                // the user pressed the back button.
+            }
+        }
+    }
+
+    private static Spanned formatPlaceDetails(Resources res, CharSequence name, String id,
+                                              CharSequence address, CharSequence phoneNumber, Uri websiteUri) {
+        Log.e(TAG, res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+        return Html.fromHtml(res.getString(R.string.place_details, name, id, address, phoneNumber,
+                websiteUri));
+
+    }
+
+    private String getCityNameByCoordinates(double lat, double lon) throws IOException {
+
+        List<Address> addresses = mGeocoder.getFromLocation(lat, lon, 1);
+        if (addresses != null && addresses.size() > 0) {
+
+            return addresses.get(0).getLocality();
+
+        }
+        return null;
+    }
+
 }
+
